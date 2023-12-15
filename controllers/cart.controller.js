b9193c30428074cc3a51da2342c6dd9716e342c7
Sivakkumar <kannantecht@gmail.com>
2023-12-15 14:43:17 +0530

    Added controller logic for the cart and updated the cart routes with middleware

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -3,7 +3,7 @@ const Product = db.product;
 const Cart = db.cart;
 const Op = db.Sequelize.Op;
 
-exports.create = (req, res) => {
+exports.createCart = (req, res) => {
   const cart = {
     userId: req.userId, //we are getting this from the middleware, verifyJwt
   };
@@ -20,11 +20,17 @@ exports.create = (req, res) => {
     });
 };
 
-exports.update = (req, res) => {
+exports.updateCart = (req, res) => {
   const cartId = req.params.id;
-
   Cart.findByPk(cartId)
     .then((cart) => {
+      if (cart == null) {
+        res.status(404).send({
+          message: "Cart doesn't exist",
+        });
+        return;
+      }
+
       Product.findAll({
         where: {
           id: {
@@ -94,4 +100,80 @@ exports.update = (req, res) => {
     });
 };
 
-exports.getCart = (req, res) => {};
+exports.getCart = (req, res) => {
+  console.log("Fetching cart for ID:", req.params.cartId);
+  Cart.findByPk(req.params.cartId)
+    .then((cart) => {
+      // console.log(cart + "----------------------------");
+      if (cart == null) {
+        res.status(404).send({
+          message: "Cart doesn't exist",
+        });
+        return;
+      }
+      // console.log(cart + "----------------------------");
+      var cost = 0;
+      const productsSelected = [];
+      cart
+        .getProducts()
+        .then((products) => {
+          for (let i = 0; i < products.length; i++) {
+            cost = cost + products[i].cost;
+            productsSelected.push({
+              id: products[i].id,
+              name: products[i].name,
+              cost: products[i].cost,
+            });
+          }
+
+          res.status(200).send({
+            id: cart.id,
+            productsSelected: productsSelected,
+            cost: cost,
+          });
+        })
+        .catch((err) => {
+          console.log(`err: ${err.message}`);
+          res.status(500).send({
+            message:
+              "Some internal server error happened while fetching cart details",
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(`err: ${err.message}`);
+      res.status(500).send({
+        message:
+          "Some internal server error happened while fetching cart details",
+      });
+    });
+};
+
+exports.deleteCart = async (req, res) => {
+  try {
+    const cartId = req.params.cartId;
+    const cart = await Cart.findByPk(cartId);
+    if (cart == null) {
+      res.status(404).send({
+        message: "Requested cart is not available",
+      });
+      return;
+    }
+
+    await Cart.destroy({
+      where: {
+        id: cartId,
+      },
+    });
+
+    res.status(200).send({
+      message: "product deleted successfully",
+    });
+  } catch (err) {
+    console.log("Error Message: " + err.message);
+    res.status(500).send({
+      message:
+        "Somme internal server error while deleting the cart based on id",
+    });
+  }
+};
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,20 +2,24 @@ const { authJwt } = require("../middleswares");
 const cartController = require("../controllers/cart.controller");
 
 module.exports = function (app) {
-  app.post("/ecomm/api/v1/carts", [authJwt.verifyToken], cartController.create);
+  app.post(
+    "/ecomm/api/v1/carts",
+    [authJwt.verifyToken],
+    cartController.createCart
+  );
   app.put(
     "/ecomm/api/v1/carts/:id",
     [authJwt.verifyToken],
-    cartController.update
+    cartController.updateCart
   );
   app.get(
     "/ecomm/api/v1/carts/:cartId",
     [authJwt.verifyToken],
     cartController.getCart
   );
-  //   app.delete(
-  //     "/ecomm/api/v1/cart",
-  //     [authJwt.verifyToken],
-  //     cartController.delete
-  //   );
+  app.delete(
+    "/ecomm/api/v1/carts/:cartId",
+    [authJwt.verifyToken],
+    cartController.deleteCart
+  );
 };
