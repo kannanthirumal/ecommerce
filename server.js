29d6155fc57652c3ed10b60718eb50ab5affe9b8
Sivakkumar <kannantecht@gmail.com>
2023-12-15 22:03:57 +0530

    Added cors package and used cors middleware for cross origin requests

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require("express");
+const cors = require("cors");
 
 const serverConfig = require("./configs/server.config");
 const bodyParser = require("body-parser");
 
 const app = express();
+/**cors - one domain only */
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    optionsSuccessStatus: 200,
+  })
+);
+
+/**cors multiple domain */
+
+// const allowedOrigins = [
+//   "http://localhost:5500",
+//   "https://yourfrontenddomain.com",
+// ];
+
+/**custom origin validation function */
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// }));
+
+/**Simple - Array of Allowed Origins: */
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//   })
+// );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
